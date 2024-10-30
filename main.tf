93b496edf5b47fdd7419e22ba6783102adcaad9a
Gurkamal Buttar <baljitk024@gmail.com>
2024-10-30 02:20:03 -0400

    terraform file - add egress

--- main.tf
+++ main.tf
@@ -24,11 +24,11 @@ resource "aws_security_group" "sg_terraform" {
         protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]  # Allow SSH from anywhere (can be restricted)
     }
-    ingress {
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]  # Allow all outgoing traffic
     }
     
     tags = {
